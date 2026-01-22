0baed54e4fbd01ea00365cbbb7d54f7bd71aa4c1
Tristan <ticarter11@icloud.com>
2026-01-22 01:49:21 +0000

    Iterate on sysfs-poll.c

--- src/app/shared/commands/configure/sysfs-poll.c
+++ src/app/shared/commands/configure/sysfs-poll.c
@@ -6,6 +6,12 @@
 
 #define NAME "sysfs-poll"
 
+#include "../../../platform/fd_file_util.h"
+
+#include <errno.h>
+#include <stdio.h>
+#include <linux/capability.h>
+
 #define VERY_HIGH_VAL 1000000000U
 
 static char const setting_napi_defer_hard_irqs[] = "napi_defer_hard_irqs";
@@ -16,13 +22,13 @@ static char const setting_irq_suspend_timeout[] = "irq_suspend_timeout";
 
 static int
 enabled( config_t * config ) {
-    return !strcmp( config->tiles.xdp.poll_mode, "pref_busy" );
+    return !strcmp( config->net.xdp.poll_mode, "pref_busy" );
 }
 
 static void
-init_perm ( fd_caps_ctx * caps,
-            config_t    * FD_PARAM_UNUSED ) {
-    fd_caps_check_capability( caps, NAME, CAP_NET_ADMIN, "configure preferred busy polling via `/sys/class/net/*/{napi_defer_hard_irqs, gro_flush_timeout, irq_suspend_timeout}`" );
+init_perm ( fd_cap_chk_t   * chk,
+            config_t const * FD_PARAM_UNUSED ) {
+    fd_cap_chk_cap( chk, NAME, CAP_NET_ADMIN, "configure preferred busy polling via `/sys/class/net/*/{napi_defer_hard_irqs, gro_flush_timeout, irq_suspend_timeout}`" );
 }
 
 static void
@@ -32,41 +38,41 @@ sysfs_net_set( char const * device,
     char path[ PATH_MAX ];
     fd_cstr_printf_check( path, PATH_MAX, NULL, "/sys/class/net/%s/%s", device, setting );
     FD_LOG_NOTICE(( "RUN: `echo \"%u\" > %s`", value, path ));
-    write_uint_file( path, value );
+    fd_file_util_write_uint( path, value );
 }
 
 static void
-init( config_t * config ) {
-    sysfs_net_set( config->tiles.net.interface, setting_napi_defer_hard_irqs, VERY_HIGH_VAL );
-    sysfs_net_set( config->tiles.net.interface, setting_gro_flush_timeout,    VERY_HIGH_VAL );
-    sysfs_net_set( config->tiles.net.interface, setting_irq_suspend_timeout,  VERY_HIGH_VAL );
+init( config_t const * config ) {
+    sysfs_net_set( config->net.interface, setting_napi_defer_hard_irqs, VERY_HIGH_VAL );
+    sysfs_net_set( config->net.interface, setting_gro_flush_timeout,    VERY_HIGH_VAL );
+    sysfs_net_set( config->net.interface, setting_irq_suspend_timeout,  VERY_HIGH_VAL );
 }
 
 static void
-fini( config_t * config,
-      int        pre_init FD_PARAM_UNUSED ) {
-    sysfs_net_set( config->tiles.net.interface, setting_napi_defer_hard_irqs, 0U );
-    sysfs_net_set( config->tiles.net.interface, setting_gro_flush_timeout,    0U );
-    sysfs_net_set( config->tiles.net.interface, setting_irq_suspend_timeout,  0U );
+fini( config_t const * config,
+      int              pre_init FD_PARAM_UNUSED ) {
+    sysfs_net_set( config->net.interface, setting_napi_defer_hard_irqs, 0U );
+    sysfs_net_set( config->net.interface, setting_gro_flush_timeout,    0U );
+    sysfs_net_set( config->net.interface, setting_irq_suspend_timeout,  0U );
 }
 
 static configure_result_t
-check( config_t * config ) {
+check( config_t const * config ) {
     static char const enoent_msg[] = "Interface not found or XDP preferred busy polling not supported:";
 
     char path[ PATH_MAX ];
-    fd_cstr_printf_check( path, PATH_MAX, NULL, "/sys/class/net/%s/%s", config->tiles.net.interface, setting_napi_defer_hard_irqs );
-    if( read_uint_file( path, enoent_msg ) != VERY_HIGH_VAL ) {
+    fd_cstr_printf_check( path, PATH_MAX, NULL, "/sys/class/net/%s/%s", config->net.interface, setting_napi_defer_hard_irqs );
+    if( fd_file_util_read_uint( path, enoent_msg ) != VERY_HIGH_VAL ) {
         NOT_CONFIGURED("Setting napi_defer_hard_irqs failed.");
     }
 
-    fd_cstr_printf_check( path, PATH_MAX, NULL, "/sys/class/net/%s/%s", config->tiles.net.interface, setting_gro_flush_timeout );
-    if( read_uint_file( path, enoent_msg ) != VERY_HIGH_VAL ) {
+    fd_cstr_printf_check( path, PATH_MAX, NULL, "/sys/class/net/%s/%s", config->net.interface, setting_gro_flush_timeout );
+    if( fd_file_util_read_uint( path, enoent_msg ) != VERY_HIGH_VAL ) {
         NOT_CONFIGURED("Setting gro_flush_timeout failed.");
     }
     
-    fd_cstr_printf_check( path, PATH_MAX, NULL, "/sys/class/net/%s/%s", config->tiles.net.interface, setting_irq_suspend_timeout );
-    if( read_uint_file( path, enoent_msg ) != VERY_HIGH_VAL ) {
+    fd_cstr_printf_check( path, PATH_MAX, NULL, "/sys/class/net/%s/%s", config->net.interface, setting_irq_suspend_timeout );
+    if( fd_file_util_read_uint( path, enoent_msg ) != VERY_HIGH_VAL ) {
         NOT_CONFIGURED("Setting irq_suspend_timeout failed.");
     }
 
